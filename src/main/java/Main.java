a2eb4fa9785e60b6a609401b1460eb3b3d383671
Arne <a.vaneycken@gmail.com>
2021-09-13 14:41:48 +0200

    better xpath using data-testid="available-tickets-list"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.setProperty("webdriver.edge.driver", "D:\\Ruby30-x64\\bin\\msedgedriver.exe");
 
         WebDriver driver = new EdgeDriver();
-        driver.get("https://browser.ticketswap.com/");
+        driver.get("https://browser.ticketswap.com/magic-link/69a1f8c6abd992c852d91188a0b2650c6006db81?locale=en&platform=web&flow=authentication");
 
         boolean gevonden = false;
         long counter = 0L;
@@ -25,7 +25,7 @@ public class Main {
             System.out.print("Refresh nummer: "+counter);
             driver.get("https://www.ticketswap.com/event/boom-festival-2022/regular/35812929-0839-4edf-803c-a69261e939b4/1463104");
             //driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
-            List<WebElement> myLinks = driver.findElements(By.xpath("/html/body/div[1]/div[2]/div[6]/ul/li[1]/a"));
+            List<WebElement> myLinks = driver.findElements(By.xpath("//ul[@data-testid='available-tickets-list']/li[1]/a"));
             System.out.println(myLinks);
             if (myLinks.size() > 0){
                 driver.get(myLinks.get(0).getAttribute("href"));
