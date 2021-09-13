e365268b0a5b33ac9d853db0ec3bf5c3eeb895bd
Arne <a.vaneycken@gmail.com>
2021-09-13 11:04:41 +0200

    edit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,14 +15,17 @@ public class Main {
         driver.get("https://www.ticketswap.com");
 
         boolean gevonden = false;
+        long counter = 0L;
         while (!gevonden) {
-            driver.get("https://www.ticketswap.com/event/boom-festival-2022/regular/35812929-0839-4edf-803c-a69261e939b4/1463104");
+            counter++;
+            driver.get("https://www.ticketswap.com/event/born-bad-evening-vox-low-le-villejuif-underground-bryans-magic-tears/regular-tickets/32ae0587-064a-4cb2-b600-1f2a562f10bc/1695046");
             var myLinks = driver.findElements(By.xpath("/html/body/div[1]/div[3]/div[3]/ul/li[1]/a"));
             System.out.println(myLinks);
             if (myLinks.size() > 0){
                 driver.get(myLinks.get(0).getAttribute("href"));
                 driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/div/div/form/div[3]/button")).click();
                 gevonden = true;
+                System.out.println("Ticket gevonden na "+counter+" keer refreshen.");
             }
         }
     }
